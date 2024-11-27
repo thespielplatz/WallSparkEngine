013f79a24837eab37b058dd980ef830850ade6cd
thespielplatz <informatics@gmx.net>
2024-11-27 10:15:41 +0100

    style(basic-clock): change colors of tsp

--- examples/shared/src/logos/TheSpielplatzLogo.ts
+++ examples/shared/src/logos/TheSpielplatzLogo.ts
@@ -2,10 +2,10 @@ import PixelBuffer from '@tsp/wse/GameEngine/drawing/PixelBuffer'
 import GameObject from '@tsp/wse/GameObjects/GameObject'
 
 import ILogo from '@shared/logos/ILogo'
-import { BLACK } from '@tsp/wse/GameEngine/drawing/colors'
 
-const GRAY = 0x4a4a4a
+const GRAY = 0xA0A0A0
 const YELLOW = 0xffc800
+const DARK = 0x202020
 
 export default class TheSpielplatzLogo extends GameObject implements ILogo {
   get width(): number {
@@ -29,22 +29,22 @@ export default class TheSpielplatzLogo extends GameObject implements ILogo {
     pixelBuffer.setPixel({ x: 3, y: 1, color: YELLOW })
     pixelBuffer.setPixel({ x: 4, y: 1, color: YELLOW })
 
-    pixelBuffer.setPixel({ x: 0, y: 2, color: BLACK })
+    pixelBuffer.setPixel({ x: 0, y: 2, color: DARK })
     pixelBuffer.setPixel({ x: 1, y: 2, color: GRAY })
-    pixelBuffer.setPixel({ x: 2, y: 2, color: BLACK })
-    pixelBuffer.setPixel({ x: 3, y: 2, color: BLACK })
-    pixelBuffer.setPixel({ x: 4, y: 2, color: BLACK })
+    pixelBuffer.setPixel({ x: 2, y: 2, color: DARK })
+    pixelBuffer.setPixel({ x: 3, y: 2, color: DARK })
+    pixelBuffer.setPixel({ x: 4, y: 2, color: DARK })
 
-    pixelBuffer.setPixel({ x: 0, y: 3, color: BLACK })
+    pixelBuffer.setPixel({ x: 0, y: 3, color: DARK })
     pixelBuffer.setPixel({ x: 1, y: 3, color: GRAY })
-    pixelBuffer.setPixel({ x: 2, y: 3, color: BLACK })
+    pixelBuffer.setPixel({ x: 2, y: 3, color: DARK })
     pixelBuffer.setPixel({ x: 3, y: 3, color: YELLOW })
-    pixelBuffer.setPixel({ x: 4, y: 3, color: BLACK })
+    pixelBuffer.setPixel({ x: 4, y: 3, color: DARK })
 
-    pixelBuffer.setPixel({ x: 0, y: 4, color: BLACK })
-    pixelBuffer.setPixel({ x: 1, y: 4, color: BLACK })
-    pixelBuffer.setPixel({ x: 2, y: 4, color: BLACK })
+    pixelBuffer.setPixel({ x: 0, y: 4, color: DARK })
+    pixelBuffer.setPixel({ x: 1, y: 4, color: DARK })
+    pixelBuffer.setPixel({ x: 2, y: 4, color: DARK })
     pixelBuffer.setPixel({ x: 3, y: 4, color: YELLOW })
-    pixelBuffer.setPixel({ x: 4, y: 4, color: BLACK })
+    pixelBuffer.setPixel({ x: 4, y: 4, color: DARK })
   }
 }
