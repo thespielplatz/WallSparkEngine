8bda576396f74eba751796834ce7bea6eb4b82e4
thespielplatz <informatics@gmx.net>
2025-03-22 21:50:03 +0100

    refactor: change console renderer to ansi-escapes

--- src/GameEngine/renderer/ConsoleRenderer.ts
+++ src/GameEngine/renderer/ConsoleRenderer.ts
@@ -1,24 +1,21 @@
-import Jetty from 'jetty'
+import ansiEscapes from 'ansi-escapes'
 
 import AbstractRenderer from '@tsp/wse/GameEngine/renderer/AbstractRenderer'
 
+const CHAR_WIDTH = 3
+const RESET_COLOR = '\x1b[0m'
 export default class ConsoleRenderer extends AbstractRenderer {
-  private jetty: Jetty
-
   constructor(params: { width: number, height: number, brightness: number }) {
     super(params)
 
-    this.jetty = new Jetty(process.stdout)
-    this.jetty.reset().clear().moveTo([0,0])
+    process.stdout.write(ansiEscapes.eraseScreen)
   }
 
   override async render(pixelData: number[]) {
     for (let i = 0; i < pixelData.length; i++) {
-      let charWidth = 0
       let x = 0
       let y = 0
 
-      charWidth = 3
       x = i % this.width
       y = Math.floor(i / this.width)
 
@@ -27,13 +24,17 @@ export default class ConsoleRenderer extends AbstractRenderer {
         continue
       }
 
-      const r = Math.floor(((0xFF0000 & c) >> 16) / 255 * 5) * this.brightness / 255
-      const g = Math.floor(((0xFF00 & c) >> 8) / 255 * 5) * this.brightness / 255
-      const b = Math.floor(((0xFF & c)) / 255 * 5) * this.brightness / 255
+      const r = (0xFF0000 & c) >> 16 * this.brightness / 255
+      const g = (0xFF00 & c) >> 8 * this.brightness / 255
+      const b = 0xFF & c * this.brightness / 255
+      const colorCode = `\x1b[38;2;${r};${g};${b}m`
 
-      this.jetty.moveTo([y, x * charWidth]).rgb([r,g,b]).text(`##${charWidth === 3 ? ' ': ''}`)
+      process.stdout.write(ansiEscapes.cursorTo(x * CHAR_WIDTH, y) + colorCode + '## ' + RESET_COLOR)
+      if (x === this.width - 1) {
+        process.stdout.write(ansiEscapes.eraseEndLine)
+      }
     }
 
-    this.jetty.moveTo([pixelData.length / this.width, 0]).rgb([5,5,5])
+    process.stdout.write(ansiEscapes.cursorNextLine)
   }
 }
